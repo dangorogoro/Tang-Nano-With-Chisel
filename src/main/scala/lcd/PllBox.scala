41e92045e24eb42f48c3cad3c98cf4d83cf48433
dangorogoro <dangorogoro0802@gmail.com>
2020-11-11 20:26:55 +0900

    Update blackbox

--- src/main/scala/lcd/PllBox.scala
+++ src/main/scala/lcd/PllBox.scala
@@ -4,11 +4,11 @@ package lcd
 import chisel3._
 import chisel3.util._
 
-class PllBox extends BlackBox with HasBlackBoxResource {
+class RPLL extends BlackBox with HasBlackBoxResource {
   val io = IO(new Bundle() {
-    val clkin = Input(Bool())    
+    val clkin = Input(Clock())    
     val clkout = Output(Bool())
     val clkoutd = Output(Bool())
   })
-  setResource("/gowin_pll.v")
+  setResource("/rpll.v")
 }
